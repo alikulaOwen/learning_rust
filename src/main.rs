63b09013186555c77e291b1e686c7d4571d28012
Caldewood Owen Alikula <alikulaowen@gmail.com>
2024-08-11 16:19:52 +0300

    start: chapter11.3 use declaration/keyword

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod my{
+mod my {
     pub struct OpenBox<T> {
         pub contents: T,
     }
@@ -9,22 +9,44 @@ mod my{
 
     impl<T> ClosedBox<T> {
         pub fn new(contents: T) -> ClosedBox<T> {
-            ClosedBox {
-                contents: contents,
-            }
+            ClosedBox { contents: contents }
+        }
+    }
+}
+
+use deeeply::nested::function as other_function;
+
+fn function() {
+    println!("called `function()`");
+}
+
+mod deeeply {
+    pub mod nested {
+        pub fn function() {
+            println!("called `deeply::nested::function()`");
         }
     }
 }
 
 fn main() {
-
-    let open_box = my::OpenBox { contents: "public information" };
+    let open_box = my::OpenBox {
+        contents: "public information",
+    };
 
     println!("The open box contains: {}", open_box.contents);
 
-    let closed_box = my::ClosedBox::new("classified information");
+    let _closed_box = my::ClosedBox::new("classified information");
 
     // Error! `contents` is private
     //println!("The closed box contains: {}", closed_box.contents);
-  
+
+    other_function();
+
+    println!("Entering block");
+    {
+        use crate::deeeply::nested::function;
+        function();
+
+        println!("Leaving block");
+    }
 }
