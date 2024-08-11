76974151c7363efe8dacc17409868f511a513aca
Caldewood Owen Alikula <alikulaowen@gmail.com>
2024-08-11 16:11:54 +0300

    visibility

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,90 @@
-fn is_odd(n: u32) -> bool {
-    n % 2 != 0
+mod my_module {
+    fn private_fn(){
+        println!("This is a private function");
+    }
+
+    pub fn public_fn(){
+        println!("This is a public function");
+    }
+
+
+    pub fn indirect_access(){
+        println!("This is an indirect access");
+        private_fn();
+    }
+
+    pub mod nested {
+        pub fn function(){
+            println!("This is a nested function");
+        }
+        // This function is private
+        pub fn private_fn(){
+            println!("This is a private nested function");
+        }
+
+        pub(in crate::my_module) fn public_fn_in_my_module(){
+            println!("This is a public nested function called from my_module my_mod::nested::public_function_in_super_mod}} ")
+        }
+
+        pub(crate) fn public_fn_in_nested(){
+            println!("This is a public nested function called from my_module my_mod::nested::public_function_in_nested}} ")
+        }
+
+        pub(super) fn public_fn_in_super_mod(){
+            println!("This is a public nested function called from my_module my_mod::nested::public_function_in_super_mod}} ")
+        }
+
+    }
+    
+    pub fn call_public_fn_in_my_module(){
+        println!("This is a public function called from my_module");
+        nested::public_fn_in_my_module();
+        nested::public_fn_in_nested();
+        nested::public_fn_in_super_mod();
+    }
+
+
+    pub(crate) fn public_fn_in_crate(){
+        println!("This is a public function called from my_module");
+    }
+
+    mod private_nested {
+        #[allow(dead_code)]
+        pub fn function(){
+            println!("This is a private nested function");
+        }
+
+        #[allow(dead_code)]
+        pub (crate) fn restricted_function(){
+            println!("This is a restricted function");
+        }
+    }
+
+
+}
+fn function(){
+    println!("This is a function");
 }
 
+pub fn public_function(){
+    println!("This is a public function");
+}
 fn main() {
-    println!("Find sum of sll squared numbers in list: ",);
-    let numbers = vec![1, 2, 3, 4, 5];
-    let mut sum_of_squares = 0;
 
-    // Iterating over the list and find odd numbers
+   function();
+    public_function();
+    my_module::public_fn();
+    my_module::indirect_access();
 
-    for &n in &numbers {
-        let n_squared = n * n;
+    my_module::nested::function();
+    my_module::call_public_fn_in_my_module();
+    my_module::public_fn_in_crate();
+    //my_module::nested::public_fn_in_my_module();
 
-        if is_odd(n_squared) {
-            sum_of_squares += n_squared;
-        }
-    }
-    println!(
-        "The sum of the squares of the odd numbers is: {}",
-        sum_of_squares
-    );
+    // my_module::nested::public_fn_in_nested();
 
-    // functional approach
+    // my_module::nested::public_fn_in_super_mod();
 
-    let sum_2: u32 = numbers.iter().map(|&n| n * n).filter(|&n| is_odd(n)).sum();
-
-    println!("The sum of the squares of the odd numbers is: {}", sum_2);
-    fn sum_odd(upto: u32) -> u32 {
-        let mut acc = 0;
-        for i in 0..upto {
-            let addition: u32 = match i % 2 != 0 {
-                true => i * i,
-                false => 0,
-            };
-            acc += addition;
-
-            /* `u32` value */
-        }
-        acc
-    }
-
-    println!(
-        "The sum of the squares of the odd numbers is: {}",
-        sum_odd(5)
-    );
-
-    // functional approach,  thattakes in a number and returns the sum of the squares of the odd numbers up to and including that number
-    fn sum_odd_2(upto: u32) -> u32 {
-        (0..upto).map(|n| n * n).filter(|&n| n % 2 != 0).sum()
-    }
-
-    print!(
-        "The sum of the squares of the odd numbers is: {}",
-        sum_odd_2(5)
-    );
+    // my_module::nested::private_fn(); 
+   
 }
