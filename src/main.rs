cbd778c25a4fb1bfb6642e55650291f0a7970c7c
Caldewood Owen Alikula <alikulaowen@gmail.com>
2024-08-13 18:30:21 +0300

    Generics chapter definations

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
-mod rary;
+
+struct A;
+
+struct Single(A);
+
+struct SingleGen<T>(T);
 
 fn main() {
-   println!("Hello, world!");
-    rary::public_function(); 
+    println!("Hello, world!");
+
+
+    let _s = Single(A);
+
+    let _char: SingleGen<char> = SingleGen('a');
+
+    let _t = SingleGen(A); // uses A
+    let _i32 = SingleGen(6); // uses i32
+    let _char = SingleGen('a'); // uses char
+
 }
+
