ead2bc0df135ba3d9b4dd6f70247e7857c4068fb
Caldewood Owen Alikula <alikulaowen@gmail.com>
2024-08-20 04:46:27 +0300

    multiple bound

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,22 @@
-struct Cardinal;
+use std::fmt::{Debug, Display};
 
-struct BlueJay;
-
-struct Turkey;
-
-trait Red {
-    
-}
-trait Blue{}
-
-impl Red for Cardinal {
-    
-}
-
-impl Blue for BlueJay{
+fn compare_prints<T: Debug + Display>(t: &T){
+    println!("Debug: `{:?}`", t);
+    println!("Display  `{}`", t);
 
 }
+ fn compare_types<T: Debug, U: Debug>(t: &T, u: &U){
+    println!("t: `{:?}`", t);
+    println!("u: `{:?}`", u);
+ }
 
-fn red <T: Red>(_: &T) -> &'static str{"red"}
-fn blue<T: Blue>(_: & T) -> &'static str{"blue"}
+ fn main() {
+    let string = "Words";
+    let array = [1,2,4];
 
-fn main(){
-    let cardinal = Cardinal;
-    let blue_jay = BlueJay;
-    let _turkey = Turkey;
+    let vec =  vec![1, 2 ,3];
 
+    compare_prints(&string);
 
-    // red wont on blue_jay because it is bound to blue
-
-    println!("Cardinal is {}", red(&cardinal));
-    println!("A blue jay is  {}", blue(&blue_jay));
-}
+    compare_types(&array, &vec);
+ }
