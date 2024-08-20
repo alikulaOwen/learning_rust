72a429eca014172ae3f19d5f2c67a76261be5008
Caldewood Owen Alikula <alikulaowen@gmail.com>
2024-08-20 04:35:41 +0300

    BOUNDS

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
-struct Empty;
-struct Null;
+struct Cardinal;
 
-trait DoubleDrop<T> {
-    fn double_drop(self, _: T);
+struct BlueJay;
+
+struct Turkey;
+
+trait Red {
+    
+}
+trait Blue{}
+
+impl Red for Cardinal {
+    
 }
 
-impl<T, U> DoubleDrop<T> for U {
-    fn double_drop(self, _: T) {
-        println!("Dropping!");
-    }
-}
-fn main() {
-    let empty = Empty;
-    let null = Null;
+impl Blue for BlueJay{
 
-    empty.double_drop(null);
+}
+
+fn red <T: Red>(_: &T) -> &'static str{"red"}
+fn blue<T: Blue>(_: & T) -> &'static str{"blue"}
+
+fn main(){
+    let cardinal = Cardinal;
+    let blue_jay = BlueJay;
+    let _turkey = Turkey;
+
+
+    // red wont on blue_jay because it is bound to blue
+
+    println!("Cardinal is {}", red(&cardinal));
+    println!("A blue jay is  {}", blue(&blue_jay));
 }
